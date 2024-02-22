66ef6d6c370c02524d16487b9c683caf7b50685c
leort11 <leonardo.figueiredo31@hotmail.com>
2024-02-22 12:41:24 -0300

    ✅Adicionando o comando /cargos com cargos depravados do servidor de um amigo... tambem foi adicionado o arquivo para guardar links uteis para o desenvolvimento do projeto

--- src/main.py
+++ src/main.py
@@ -7,6 +7,10 @@ TOKEN = config('TOKEN', default='')
 PREFIX = config('PREFIX', default='/')
 IDAUTHOR = config('IDAUTHOR', default='')
 
+red = 0xC70039
+msg_id = None
+msg_user = None
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -35,7 +39,133 @@ async def moeda_command(ctx):
     elif choice == 2:
         await ctx.message.add_reaction('👑')
 
-        
+@bot.command(name='cargos')
+async def cargos_command(ctx):
+    embed = discord.Embed(
+        title="Escolha seu cargo FODA!!",
+        color=red,
+        description="- Goza Fino  = 🤓\n"
+                    "- Goza Grosso  = 😎\n"
+                    "- Cintura Ignorante  = 🥵\n"
+                    "- Cheira Peido  = 💩\n"
+                    "- LÍNGUA PRETA  = 👅\n"
+                    "- Adora Leite  = 🍼\n"
+                    "- Tem Amizade com o Patrão?  = 👥\n"
+                    "- Beija bem?  = 💋\n"
+                    "- Faz Gostoso?  = 🤨\n"
+                    "- BRUHH  = ☣️\n",
+    )
+    botmsg = await ctx.send(embed=embed)
+    await botmsg.add_reaction("🤓")
+    await botmsg.add_reaction("😎")
+    await botmsg.add_reaction("🥵")
+    await botmsg.add_reaction("💩")
+    await botmsg.add_reaction("👅")
+    await botmsg.add_reaction("🍼")
+    await botmsg.add_reaction("👥")
+    await botmsg.add_reaction("💋")
+    await botmsg.add_reaction("🤨")
+    await botmsg.add_reaction("☣️")
+
+    global msg_id
+    msg_id = botmsg.id
+    global msg_user
+    msg_user = ctx.author
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    msg = reaction.message
+    guild = user.guild
+
+    if reaction.emoji == "🤓" and msg.id == msg_id:
+        role = discord.utils.get(guild.roles, name="Goza Fino")
+        member = guild.get_member(user.id)
+        await member.add_roles(role)
+        print("adicionou")
+
+    if reaction.emoji =="😎" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Goza Grosso", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="🥵" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Cintura Ignorante", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="💩" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Cheira Peido", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="👅" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "LÍNGUA PRETA", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="🍼" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Adora Leite", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="👥" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Tem Amizade com o Patrão?", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="💋" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Beija bem?", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="🤨" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Faz Gostoso?", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="☣️" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "BRUHH", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+@bot.event
+async def on_reaction_remove(reaction, user):
+    msg = reaction.message
+    guild = user.guild
+
+    if reaction.emoji == "🤓" and msg.id == msg_id:
+        role = discord.utils.get(guild.roles, name="Goza Fino")
+        member = guild.get_member(user.id)
+        await member.remove_roles(role)
+        print("removeu")
+
+    if reaction.emoji =="😎" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Goza Grosso", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="🥵" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Cintura Ignorante", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="💩" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Cheira Peido", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="👅" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "LÍNGUA PRETA", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="🍼" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Adora Leite", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="👥" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Tem Amizade com o Patrão?", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="💋" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Beija bem?", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="🤨" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "Faz Gostoso?", msg.guild.roles)
+        await bot.add_roles(user, role)
+
+    if reaction.emoji =="☣️" and msg.id == msg_id:
+        role = discord.utils.find(lambda r: r.name == "BRUHH", msg.guild.roles)
+        await bot.add_roles(user, role)
+
     
 if not TOKEN:
     raise ValueError("A variável de ambiente TOKEN não foi definida.")
